996174e1db712e085ef2986b1662aecc2f95cf1b
LisaMLorenz <hej@frau-lorenz.de>
2023-02-24 21:30:08 +0000

    intern submission is buggy

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 // Skeleton structure provided by class instructor Dan Mueller
+
 const employees = [];
+
 inquirer.prompt([{
     //manager questions
     type: 'input',
@@ -61,40 +63,194 @@ inquirer.prompt([{
     }
 },
 
+{
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is your office number?',
+    validate: function (input) {
+        const officeNumber = input.trim();
+        if (!officeNumber || !/^\d+$/.test(officeNumber)) {
+            return "Please enter a valid office with digits only.";
+        }
+        return true;
+    }
+},
+
 ]).then(response => {
-    const manager = new Manager(response.name, response.id, response.email);
+    const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
     employees.push(manager);
     promptForNextEmployee();
 })
 
 const promptForNextEmployee = () => {
     inquirer.prompt([{
-        // choice of 3
-    }]).then(response => {
-        // if engineer
-        //    promptForEngineer
-        // else if intern
-        //    promptForIntern
-        // else
-        //    use the functionality from page-template to generate the team
+
+        type: 'list',
+        message: 'What is your role?',
+        name: 'role',
+        choices: ['Engineer', 'Intern', 'No more team members to add']
+    },
+
+    ]).then(response => {
+        // if intern
+
+        if (response.role === 'Intern') {
+            promptForIntern(); //    promptForIntern
+        }
+
+        // else if engineer
+        if (response.role === 'Engineer') {
+            promptForEngineer(); //    promptForEngineer
+        }
+        else {
+            //    use the functionality from page-template to generate the team
+            const html = render(employees);
+            fs.writeFile(outputPath, html, function (err) {
+                if (err) throw err;
+                console.log('Team page generated!');
+            });
+        }
+
     })
 }
 
 const promptForEngineer = () => {
     inquirer.prompt([{
-        //engineer questions
-    }]).then(response => {
-        // add new engineer to employees array
-        // promptForNextEmployee
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        validate: function (input) {
+            const name = input.trim();
+            if (!name || typeof name !== "string") {
+                return "Please enter a valid name.";
+            } else if (/\d/.test(name)) { // Check if the name contains any numbers
+                return "Name cannot contain numbers.";
+            } else if (/[^\w\s]/.test(name)) { // Check if the name contains any special symbols
+                return "Name cannot contain special symbols.";
+            } else {
+
+                return true;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID number?',
+        validate: function (input) {
+            const id = input.trim();
+            if (!id || !/^\d+$/.test(id)) {
+                return "Please enter a valid ID with digits only.";
+            }
+            return true;
+        }
+    },
+
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: function (input) {
+            const email = input.trim();
+            if (!email || !email.includes("@")) {
+                return "Please enter a valid email address.";
+            }
+            return true;
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub name?',
+        validate: function (input) {
+            const github = input.trim();
+            if (!github || typeof github !== "string") {
+                return "Please enter a valid GitHub name.";
+            } else if (/[^\w\s]/.test(github)) { // Check if the name contains any special symbols
+                return "GitHub name cannot contain special symbols.";
+            } else {
+
+                return true;
+            }
+
+        },
+    }
+
+    ]).then(response => {
+        const engineer = new Engineer(response.name, response.id, response.email, response.github);
+        employees.push(engineer);
+        promptForNextEmployee();
     })
 }
 
 const promptForIntern = () => {
     inquirer.prompt([{
         //intern questions
-    }]).then(response => {
-        // add new intern to employees array
-        // promptForNextEmployee
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        validate: function (input) {
+            const name = input.trim();
+            if (!name || typeof name !== "string") {
+                return "Please enter a valid name.";
+            } else if (/\d/.test(name)) { // Check if the name contains any numbers
+                return "Name cannot contain numbers.";
+            } else if (/[^\w\s]/.test(name)) { // Check if the name contains any special symbols
+                return "Name cannot contain special symbols.";
+            } else {
+
+                return true;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID number?',
+        validate: function (input) {
+            const id = input.trim();
+            if (!id || !/^\d+$/.test(id)) {
+                return "Please enter a valid ID with digits only.";
+            }
+            return true;
+        }
+    },
+
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: function (input) {
+            const email = input.trim();
+            if (!email || !email.includes("@")) {
+                return "Please enter a valid email address.";
+            }
+            return true;
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Which is your school?',
+        validate: function (input) {
+            const email = input.trim();
+            if (!school || typeof school !== "string") {
+                return "Please enter a valid school name.";
+            }
+            return true;
+        }
+    },
+
+    ]).then(response => {
+        const intern = new Intern(response.name, response.id, response.email, response.school);
+        employees.push(intern);
+        promptForNextEmployee();
     })
 }
 
